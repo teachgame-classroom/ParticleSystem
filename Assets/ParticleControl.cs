6a07d80e6fa171ebe6ae01da56beef2d37355d98
teachgame <teachgame@163.com>
2019-06-03 11:58:39 +0800

    Init Commit

---
+++ Assets/ParticleControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private float startSpeed = 25;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(ps.isPlaying)
+            {
+                ps.Stop();
+            }
+            else if(ps.isStopped)
+            {
+                ps.Play();
+            }
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            startSpeed += Time.deltaTime * 10;
+
+            ParticleSystem.MainModule main = ps.main;
+            main.startSpeed = startSpeed;
+
+            ParticleSystem.EmissionModule em = ps.emission;
+            em.rateOverTime = startSpeed * startSpeed;
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            startSpeed -= Time.deltaTime * 10;
+
+            ParticleSystem.MainModule main = ps.main;
+            main.startSpeed = startSpeed;
+
+            ParticleSystem.EmissionModule em = ps.emission;
+            em.rateOverTime = startSpeed * startSpeed;
+        }
+    }
+
+    void OnParticleSystemStopped()
+    {
+        Debug.Log("粒子系统停止");
+    }
+}
